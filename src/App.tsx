dd43486da1fc6d8d8a11c1a96b6b9140f3311087
tianshaojian <tianshaojian@autohome.com.cn>
2024-06-27 15:16:30 +0800

    remove useless imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PokerInfo from './components/PokerInfo';
--- src/components/AppFeatures.tsx
+++ src/components/AppFeatures.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from '../styles/AppFeatures.module.css';
 
 function AppFeatures() {
